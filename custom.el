2b47853d550daf0ca986435e3a56e89c1c6c7edc
cperez-one-two <christopher12perez6@gmail.com>
2020-11-23 15:42:01 -0500

    First commit

---
+++ custom.el
@@ -0,0 +1,13 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(org-agenda-files
+   '("~/docs/org/notes/capstone-2/capstone2.org" "~/docs/org/notes/java/http-and-web-apis.org" "~/docs/org/org-basics.org")))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
